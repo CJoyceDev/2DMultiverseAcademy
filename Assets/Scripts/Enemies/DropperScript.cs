6a7cc80d5a94e44d31c800e1e65b138958885c6b
Cabhan64 <57735019+Cabhan64@users.noreply.github.com>
2024-11-18 17:35:07 +0000

    Dropper Small change
    Make drop timer public

--- Assets/Scripts/Enemies/DropperScript.cs
+++ Assets/Scripts/Enemies/DropperScript.cs
@@ -7,6 +7,8 @@ public class DropperScript : MonoBehaviour
 
     [SerializeField] public GameObject FallingEnemy;
     [SerializeField] public GameObject FallingInstance;
+    [SerializeField] public float FallTime;
+    
     Vector3 Spawn;
 
 
@@ -36,7 +38,7 @@ public class DropperScript : MonoBehaviour
     IEnumerator DropWait()
         {
             // gives time for the drop to fall before destroying it and making a new one CD
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(FallTime);
             Destroy(FallingInstance);
             Drop();
 
