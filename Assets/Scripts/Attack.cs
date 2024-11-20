971c0aa1ae05d9485f7036dccd4cb326d1829cc4
ShadowSpider125 <RSIMPS301@caledonian.ac.uk>
2024-11-20 21:32:28 +0000

    Changed Max Attack To Push Boxes

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -6,12 +6,24 @@ public class Attack : MonoBehaviour
 {
     public float LifeTime = 2;
 
+    public static float ForceX = 15f;
+    public static float ForceY = 3f;
+
+    Vector3 PushForce = new Vector3(ForceX, ForceY, 0);
+    Rigidbody rb;
+
     private void OnCollisionEnter(Collision other)
     {
         //Detects Enemy RS
         if (other.gameObject.tag == "KillBox")
         {
+            //uncomment this to test pushing enemies away instead of killing them
+            //rb = other.gameObject.GetComponent<Rigidbody>();
+            //rb.AddForce(PushForce, ForceMode.Impulse);
+
             Debug.Log("Enemy Hit");
+
+            //comment this out when testing pushing enemies away
             Destroy(other.gameObject);
             Destroy(this.gameObject);
         }
@@ -21,6 +33,14 @@ public class Attack : MonoBehaviour
         {
             Destroy(this.gameObject);
             Debug.Log("Player Hit");
+        }
+
+        if (other.gameObject.tag == "Box")
+        {
+            rb = other.gameObject.GetComponent<Rigidbody>();
+            rb.AddForce(PushForce, ForceMode.Impulse);
+
+            Destroy(this);
         }
     }
 
