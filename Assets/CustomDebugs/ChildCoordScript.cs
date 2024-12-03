d7a556b4f9070334eda4a0ff180fad6a33f06dec
CJoyceDev <coryjoyce14@gmail.com>
2024-12-03 12:32:53 +0000

    Added Way to test distances

---
+++ Assets/CustomDebugs/ChildCoordScript.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class ChildCoordScript : MonoBehaviour
+{
+    [MenuItem("Debug/Print Global Position")]
+    public static void PrintGlobalPosition()
+    {
+        if (Selection.activeGameObject != null)
+        {
+            Debug.Log(Selection.activeGameObject.name + " is at " + Selection.activeGameObject.transform.position);
+        }
+    }
+}
