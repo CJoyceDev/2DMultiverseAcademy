bf2ca4fb55dca102c19d0b6d225944990355d6f9
Cabhan64 <57735019+Cabhan64@users.noreply.github.com>
2025-02-10 18:55:30 +0000

    Knockback and health

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class PlayerHealth : MonoBehaviour
+{
+
+    public int playerStartHealth;
+    PlayerPauseUI ppUI;
+    private int playerHealth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerHealth = playerStartHealth;
+        ppUI = GameObject.FindGameObjectWithTag("CameraEmpty").GetComponent<PlayerPauseUI>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(playerHealth == 0)
+        {
+            print("kill");
+            ppUI.DeathUI();
+            
+
+        }
+
+    }
+
+    public void Hit()
+    { 
+        playerHealth = playerHealth - 1;
+    }
+
+    void Heal()
+    {
+        playerHealth = playerHealth + 1;
+    }
+}
