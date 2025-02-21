03c830ece89cd2f67e443a0c6b76894e6234e3f1
CJoyceDev <coryjoyce14@gmail.com>
2025-02-21 15:37:31 +0000

    Level Moment

---
+++ Assets/Scripts/NewPlayer/PlayerColliderTriggers.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PlayerColliderTrigger : MonoBehaviour
+{
+    Rigidbody rb;
+    PlayerPauseUI ppUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        ppUI = GameObject.FindGameObjectWithTag("CameraEmpty").GetComponent<PlayerPauseUI>();
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("HurtBox"))
+        {
+            //DamagePlayer();
+        }
+
+        if (other.CompareTag("KillBox"))
+        {
+            Kill();
+        }
+
+        if (other.CompareTag("Finish"))
+        {
+            //Win();
+        }
+
+        if (other.CompareTag("NonOOBKillBox"))
+        {
+            Kill();
+        }
+
+
+        void Kill()
+        {
+            print("kill");
+            ppUI.DeathUI();
+        }
+    }
+}
