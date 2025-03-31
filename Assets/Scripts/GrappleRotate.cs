8d256c1977dbdf53d82a498dce2e54b2245baaad
Cabhan64 <57735019+Cabhan64@users.noreply.github.com>
2025-03-31 19:00:07 +0100

    Grapple targeting fix

--- Assets/Scripts/GrappleRotate.cs
+++ Assets/Scripts/GrappleRotate.cs
@@ -9,7 +9,10 @@ public class GrappleRotate : MonoBehaviour
     public float radius = 5f; // Radius of the circle
     public int numberOfRays = 36; // Number of rays to cast
     float HitPoint;
-   // Quaternion target = Quaternion.Euler(-10, 0, 0);
+   public List<Vector3> positionsList = new List<Vector3>();
+   public Vector3[] positions;
+    Vector3 TargetSpot;
+    // Quaternion target = Quaternion.Euler(-10, 0, 0);
 
     void Update()
     {
@@ -23,29 +26,52 @@ public class GrappleRotate : MonoBehaviour
             float angle = i * Mathf.PI * 2 / numberOfRays;
             Vector3 direction = new Vector3(Mathf.Cos(angle) ,Mathf.Sin(angle), 0);
             Ray ray = new Ray(transform.position, direction);
-            RaycastHit hit;
+            RaycastHit[] hits = Physics.RaycastAll(ray, radius);
 
-            if (Physics.Raycast(ray, out hit, radius))
-            {
+            foreach (RaycastHit hit in hits)
+               {
                 if (hit.collider.CompareTag(targetTag))
                 {
                     Debug.DrawRay(transform.position, direction * hit.distance, Color.red);
                     // Point towards the hit object
-                    
-                    transform.LookAt(hit.point);
+                    positionsList.Add(hit.point);
+                     positions = positionsList.ToArray();
+                      TargetSpot = GetMeanVector(positions);
+                      transform.LookAt(TargetSpot);
                     break; // Stop after finding the first target
                 }
-                else
+                 /* else
                 {
                     Debug.DrawRay(transform.position, direction * hit.distance, Color.green);
-                }
+                }*/
+                else
+                  {
+                  //transform.rotation = target;
+                  Debug.DrawRay(transform.position, direction * radius, Color.blue);
+                     }
             }
-            else
-            {
-                //transform.rotation = target;
-                Debug.DrawRay(transform.position, direction * radius, Color.blue);
-            }
+           
+           
+
+
         }
+        positionsList.Clear();
+    }
+
+    private Vector3 GetMeanVector(Vector3[] positions)
+    {
+        if (positions.Length == 0)
+            return Vector3.zero;
+        float x = 0f;
+        float y = 0f;
+        float z = 0f;
+        foreach (Vector3 pos in positions)
+        {
+            x += pos.x;
+            y += pos.y;
+            z += pos.z;
+        }
+        return new Vector3(x / positions.Length, y / positions.Length, z / positions.Length);
     }
 
 }
