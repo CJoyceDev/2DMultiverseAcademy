105627545a26a00cdf0a216c90347108116d37d5
CJoyceDev <coryjoyce14@gmail.com>
2025-04-07 22:21:46 +0100

    Science dropper changes

---
+++ Assets/Scripts/Enemies/ScienceRollingHazardDropper1.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using UnityEngine;
+
+public class ScienceRollingHazardDropper : MonoBehaviour
+{
+    [SerializeField] private GameObject spawnpoint;
+    [SerializeField] private GameObject FallingEnemy1;
+    [SerializeField] private GameObject FallingEnemy2;
+    [SerializeField] private GameObject FallingEnemy3;
+    [SerializeField] private GameObject FallingEnemy4;
+    [SerializeField] private float FallTime;
+    [SerializeField] private float SpawnDelay = 2f;
+    GameObject randomFallingEnemy;
+
+    
+
+  
+
+
+    void Start()
+    {
+        StartCoroutine(SpawnHazards());
+    }
+
+    IEnumerator SpawnHazards()
+    {
+        while (true) 
+        {
+            int randomIndex = Random.Range(0, 4); 
+
+            if (randomIndex == 0)
+            {
+                randomFallingEnemy = FallingEnemy1;
+            }
+            else if (randomIndex == 1)
+            {
+                randomFallingEnemy = FallingEnemy2;
+            }
+            else if (randomIndex == 2)
+            {
+                randomFallingEnemy = FallingEnemy3;
+            }
+            else
+            {
+                randomFallingEnemy = FallingEnemy4;
+            }
+
+            GameObject newHazard = Instantiate(randomFallingEnemy, spawnpoint.transform.position, Quaternion.Euler(-90f, 0f, 0f));
+            StartCoroutine(DestroyAfterTime(newHazard, FallTime));
+            yield return new WaitForSeconds(SpawnDelay);
+        }
+    }
+
+    IEnumerator DestroyAfterTime(GameObject hazard, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(hazard);
+    }
+}
+
